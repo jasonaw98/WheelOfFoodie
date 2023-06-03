1d4d1ed59484924adee277f73b2e6eaf713a010e
Jason <jason.aw@attspace.ventures>
2023-06-04 02:33:16 +0800

    update

--- app/page.js
+++ app/page.js
@@ -1,14 +1,12 @@
 'use client'
 import React, { useState } from "react";
 import WheelOfFood from "./wheel";
-import WheelOfFortune from "./bard";
 
 
 export default function Home() {
     return(
       <div>
         <WheelOfFood/>
-        {/* <WheelOfFortune/> */}
       </div>
     )
   }
